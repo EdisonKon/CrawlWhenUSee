3f3ee208578dea45b10e06b665902ec1e5290ca9
EdisonKon <huainian_cl@126.com>
2018-04-19 10:51:32 +0800

    first commit to show gif

---
+++ src/main/java/com/example/crawlwhenusee/CrawlwhenuseeApplication.java
@@ -0,0 +1,12 @@
+package com.example.crawlwhenusee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlwhenuseeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlwhenuseeApplication.class, args);
+	}
+}
